62bbcd44a84150a61d9a1dd9d4abc7592aca231d
dhruvalgupta26 <dhruval.gupta@innobitsystems.com>
2024-09-12 10:57:42 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
+
+# load the pdf
+loader = PyPDFLoader("/documents/PERSONAL LOAN APPLICATION FORM.pdf")
+document = loader.load()
